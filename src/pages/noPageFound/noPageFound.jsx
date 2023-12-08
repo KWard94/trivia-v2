cfef244905ed0a1a7c6a09e03c6e99ac6762711f
KWard <pcward.k@gmail.com>
2023-12-08 13:58:25 -0700

    added shrug image to assets, set up basic page not found page with link to home

--- src/pages/noPageFound/noPageFound.jsx
+++ src/pages/noPageFound/noPageFound.jsx
@@ -1,22 +1,26 @@
+import { NavLink } from "react-router-dom";
+import Image from "./../../assets/images/person-shrugging-light-skin-tone.svg";
+
 import "./noPageFound.scss";
 
-function noPageFound() {
+function NoPageFound() {
   return (
     <div className="main_content container-fluid">
-      {/* <div>
-        <h1>This Page Does Not Exist, Sorry!</h1>
-      </div> */}
-      <div class="jumbotron jumbotron-fluid">
-        <div class="container">
-          <h1 class="display-4">Page Not Found</h1>
-          <p class="lead">We are sorry, this page does not exist</p>
+      <div className=" content_container">
+        <div className="container">
+          <h1 className="display-4">Page Not Found</h1>
+          <p className="lead">We are sorry, this page does not exist</p>
         </div>
-        <button type="button" class="btn btn-dark">
-          Dark
+
+        <img className="shrug" alt="Person Shrugging" src={Image} />
+        <button type="button" className="btn btn-dark">
+          <NavLink className="nav-link" to={"/"}>
+            Home
+          </NavLink>
         </button>
       </div>
     </div>
   );
 }
 
-export default noPageFound;
+export default NoPageFound;
